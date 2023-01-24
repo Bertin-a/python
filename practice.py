e558ff5e2b918ae1e1f325b8e55986d7da59f82f
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-01-24 13:34:36 +0000

    data types in python

---
+++ practice.py
@@ -0,0 +1,23 @@
+name ="Beau"
+print(type(name))
+print(type(name)==str)
+print(isinstance(name,str))
+num =1
+print(type(num))
+print(isinstance(num,int))
+print(isinstance(num,float))
+age = float(2)
+print(type(age))
+print(isinstance(age,int))
+print(isinstance(age,float))
+
+ages= int("20")
+print(type(ages))
+print(isinstance(ages,int))
+print(isinstance(ages,float))
+
+stringNumbers = "20"
+
+numbs = int(stringNumbers)
+
+print(type(numbs))
